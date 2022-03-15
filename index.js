439dce9095cb78e70a529358134d375b908e3d67
Amer Ibric <ameribric@gmail.com>
2022-03-15 12:04:14 -0400

    commit tree ab22af5d9ab44715cae9ca595693b524d0a2ba33

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = 'Amer';
+const  welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage)
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage)
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting)
